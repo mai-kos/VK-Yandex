dc5094f784e61bd4b2386f3f5154e92e6336fd3d
Konstantin <maykos94@gmail.com>
2022-12-19 22:52:55 +0300

    Initial commit

---
+++ vk_photos.py
@@ -0,0 +1,24 @@
+import requests
+from pprint import pprint
+
+class VK:
+
+   def __init__(self, access_token, user_id, version='5.131'):
+       self.token = access_token
+       self.id = user_id
+       self.version = version
+       self.params = {'access_token': self.token, 'v': self.version}
+
+   def users_info(self):
+       url = 'https://api.vk.com/method/users.get'
+       params = {'user_ids': self.id}
+       response = requests.get(url, params={**self.params, **params})
+       return response.json()
+
+
+with open('token.txt', 'rt') as file:
+    access_token = file.readline() 
+
+user_id = '8841655'
+vk = VK(access_token, user_id)
+pprint(vk.users_info())
